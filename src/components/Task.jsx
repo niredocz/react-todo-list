0f3e793d0c1e306d0650a4cefb3babfdb1338532
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-03-24 08:26:35 +0700

    default value task component

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,7 +1,6 @@
 export default function Task({ taskNameValue, inputChange, btnDelete, id }) {
   return (
     <div className="result">
-      <h2 style={{ color: "#fff", marginBottom: 0 }}>{id}</h2>
       <input
         className="result-text"
         type="text"
