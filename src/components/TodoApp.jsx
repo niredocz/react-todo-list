09eea0eda87f7df6f5b55cc71ac78b7496c51f27
Daffa Aldzakian F <daffaaldzakian20@gmail.com>
2023-03-29 09:45:49 +0700

    handleUpdateTodo, edit todoForm, fix bug re-render

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -5,7 +5,7 @@ import "./TodoApp.css"
 function TodoApp() {
   // const [todoList, setTodoList] = useState([])
   const [todoList, setTodoList] = useState(() => {
-    const savedTodos = localStorage.getItem("todoList")
+    const savedTodos = localStorage.getItem("todoStorage")
 
     if (savedTodos) {
       return JSON.parse(savedTodos)
@@ -15,10 +15,18 @@ function TodoApp() {
   })
 
   const [newTask, setNewTask] = useState("")
+  const [isEditing, setIsEditing] = useState(false)
+  const [currentTodo, setCurrentTodo] = useState({})
 
   const handleChange = (e) => {
     const value = e.target.value
     setNewTask(value)
+  }
+
+  const handleEditInputChange = (e) => {
+    setCurrentTodo({ ...currentTodo, taskName: e.target.value })
+
+    console.log(currentTodo)
   }
 
   const handleSubmit = (e) => {
@@ -26,8 +34,8 @@ function TodoApp() {
 
     if (newTask !== "") {
       const task = {
-        id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-        // id: todoList.length + 1,
+        // id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+        id: todoList.length + 1,
         taskName: newTask.trim(),
       }
 
@@ -39,39 +47,84 @@ function TodoApp() {
     setNewTask("")
   }
 
+  const handleUpdateTodo = (id, updatedTodo) => {
+    const updatedItem = todoList.map((newTask) => {
+      return newTask.id === id ? updatedTodo : newTask
+    })
+    
+    setIsEditing(false)
+    setTodoList(updatedItem)
+  }
+
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault()
+
+    handleUpdateTodo(currentTodo.id, currentTodo)
+  }
+
+  function handleEdit(todo) {
+    setIsEditing(true)
+    setCurrentTodo({ ...todo })
+  }
+
   const handleDelete = (id) => {
-    const removeItem = todoList.filter((newTask) => newTask.id !== id)
+    const removeItem = todoList.filter((todo) => todo.id !== id)
 
     setTodoList(removeItem)
   }
 
   useEffect(() => {
-    localStorage.setItem("todoList", JSON.stringify(todoList))
+    localStorage.setItem("todoStorage", JSON.stringify(todoList))
   }, [todoList])
 
   return (
     <div className="todo-wrap">
-      <h1 style={{ marginBottom: 30 }}>Todo App</h1>
-      <form autoComplete="off">
-        <input
-          type="text"
-          id="input-form"
-          value={newTask}
-          onChange={handleChange}
-          placeholder="Input Todo List ..."
-        />
-        <button onClick={handleSubmit}>+ Add Task</button>
-      </form>
+      {isEditing ? (
+        <>
+          <h1 style={{ marginBottom: 30 }}>Edit Todo App</h1>
+          <form autoComplete="off" onSubmit={handleEditFormSubmit}>
+            <input
+              type="text"
+              id="input-form"
+              value={currentTodo.taskName}
+              onChange={handleEditInputChange}
+              placeholder="Input Todo List ..."
+            />
+            <button type="submit">+ Update</button>
+            <button
+              type="button"
+              onClick={() => setIsEditing(false)}
+              style={{ backgroundColor: "red", color: "#fff" }}>
+              x Cancel
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1 style={{ marginBottom: 30 }}>Todo App</h1>
+          <form autoComplete="off" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="input-form"
+              value={newTask}
+              onChange={handleChange}
+              placeholder="Input Todo List ..."
+            />
+            <button type="submit">+ Add Task</button>
+          </form>
+        </>
+      )}
 
       <div className="list-todo">
         <h2 style={{ marginBottom: 30 }}>Todo List</h2>
         {todoList.map((newTask, i) => {
           return (
             <Task
-              key={i}
-              id={newTask.id}
+              key={newTask.id}
+              // id={newTask.id}
               taskNameValue={newTask.taskName}
               inputChange={handleChange}
+              btnEdit={() => handleEdit(newTask)}
               btnDelete={() => handleDelete(newTask.id)}
             />
           )
